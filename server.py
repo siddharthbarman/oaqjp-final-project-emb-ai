4833b6f1c3806cade5d35043df4e7c70253d858d
Siddharth R Barman <sbytestream@outlook.com>
2025-03-16 03:39:32 -0400

    Task 8: Run static code analysis completed.

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+""" Capstone project """
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,17 +6,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """ Displays the index page """
     return render_template("index.html")
 
 @app.route("/emotionDetector")
 def detect_emotion():
+    """ Detects emotion using WatsonX API """
     text_to_analyze = request.args["textToAnalyze"]
     result = emotion_detector(text_to_analyze)
-    if result['dominant_emotion'] == None:
+    if result['dominant_emotion'] is None:
         return 'Invalid text! Please try again!'
-    else:
-        return emotion_detector(text_to_analyze)
+
+    return emotion_detector(text_to_analyze)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
