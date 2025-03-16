7fa9258a89ec6b32bd411ce1975c39eb74c5eb85
Siddharth R Barman <sbytestream@outlook.com>
2025-03-16 03:32:45 -0400

    Task 7: Incorporate Error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,10 +2,17 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
+    if text_to_analyze == None or text_to_analyze == '':
+        return { 'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None }  
+    
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     request_json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = request_json, headers=headers)
+    
+    if response.status_code == 400:
+        return { 'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None }
+    
     j = json.loads(response.text)
     emotions = j['emotionPredictions'][0]['emotion']
     dominant_score = 0
--- server.py
+++ server.py
@@ -10,7 +10,12 @@ def index():
 @app.route("/emotionDetector")
 def detect_emotion():
     text_to_analyze = request.args["textToAnalyze"]
-    return emotion_detector(text_to_analyze)
+    result = emotion_detector(text_to_analyze)
+    if result['dominant_emotion'] == None:
+        return 'Invalid text! Please try again!'
+    else:
+        return emotion_detector(text_to_analyze)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
